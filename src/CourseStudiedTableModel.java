6f5337fb96b0891bb194a210b248bcb7d6d884f7
emeliehe <emelie046@gmail.com>
2018-09-26 15:31:01 +0200

    updates

--- src/CourseStudiedTableModel.java
+++ src/CourseStudiedTableModel.java
@@ -1,4 +1,43 @@
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+import assignmentOne.HasStudied;
 
 public class CourseStudiedTableModel {
+	
+	private String[] columns;
+	private String[] data;
+	
+	private CourseStudiedTableModel() {
+		this.columns = new String[] {"Ssn", "Grade" };
+	}
+	public CourseStudiedTableModel(List<Studied>)studies) {
+		this();
+		
+		this.data = new String(HasStudied.size())][];
+		for(int i=0; i<this.data.length; i++) {
+			HasStudied s = HasStudied.get(i);
+			data[i] = new String[] {s.getStudentSsn(), String.valueOf(s.getGrade())};
+		{
+	}
+	@Override
+	public int getColumnCount() {
+		return this.columns.length;
+	}
+	@Override 
+	public String getColumnName(int col) {
+		return this.columns[col];
+	}
+	@Override
+	public int getRowCount() {
+		return this.data.length;
+	}
+	@Override
+	public Object getValueAt (int rowIndex, int columnIndex) {
+		return this.data[rowIndex][columnIndex]
+	}
+}
+	
+	
 
 }
