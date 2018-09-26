a627173e7186f6fffc9ade28394faabcda4eeff6
emeliehe <emelie046@gmail.com>
2018-09-26 14:44:02 +0200

    changes

--- src/CourseStudiesTableModel.java
+++ src/CourseStudiesTableModel.java
@@ -5,7 +5,18 @@ import javax.swing.table.AbstractTableModel;
 
 public class CourseStudiesTableModel extends AbstractTableModel {
 	
-	private String columns;
-	private String data;
+	private String [] columns;
+	private String[] data;
+	
+	private CourseStudiesTableModel() {
+		this.columns = new String[] {"SSn"};
+	
+	public CourseStudiesTableModel(List<Studies> studies) {
+		this();
+		
+		this.data
+	}
+				
+	}
 
 }
