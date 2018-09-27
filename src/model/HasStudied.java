77315c9f8ec254d975717673ea1a7ddba312181d
sofiahallberg <43443852+sofiahallberg@users.noreply.github.com>
2018-09-27 13:24:24 +0200

    ändrat HasStudied

--- src/model/HasStudied.java
+++ src/model/HasStudied.java
@@ -2,15 +2,24 @@ package model;
 
 public class HasStudied {
 
-	private int studentSsn;
+	private String studentSsn;
 	private String courseCode;
 	private int grade;
 
-	public int getStudentSsn() {
+	public HasStudied() {
+		
+	}
+	public HasStudied (String studentSsn, String courseCode, int grade) {
+		this.studentSsn = studentSsn;
+		this.courseCode = courseCode;
+		this.grade = grade;
+	}
+	
+	public String getStudentSsn() {
 		return studentSsn;
 	}
 
-	public void setStudentSsn(int studentSsn) {
+	public void setStudentSsn(String studentSsn) {
 		this.studentSsn = studentSsn;
 	}
 
